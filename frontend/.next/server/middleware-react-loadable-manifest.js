70decbd82e01ddd9135d16b2a1de193495efee5a
Sidharth <kaustubh.sidharth@te.com>
2026-09-04 16:27:02 +0530

    Remove .next build artifacts and fix gitignore

--- frontend/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"pages\\\\index.js -> ../components/ParticleSphere\":{\"id\":\"pages\\\\index.js -> ../components/ParticleSphere\",\"files\":[\"static/chunks/components_ParticleSphere_jsx.js\"]}}"
